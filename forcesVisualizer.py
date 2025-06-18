7d31876e92604f38d9a9092bd42c0c5b2644bcea
Vedant Mohapatra <vedant.mohapatra@gmail.com>
2025-06-18 13:15:47 -0500

    identify mins

--- forcesVisualizer.py
+++ forcesVisualizer.py
@@ -7,10 +7,10 @@ fx_total = []
 px_total = []
 vx_total = []
 
-with open("C:/Users/Vedant/Downloads/postProcessing/postProcessing/forces1/0/force.dat", 'r') as file:
+with open("C:/Users/Vedant/Documents/BreakingWaves-Cylinder/postProcessing/forces1/0/force.dat", 'r') as file:
     for line in file:
         if line.startswith('#') or not line.strip():
-            continue  # skip comments and blank lines
+            continue 
 
         parts = line.strip().split()
         if len(parts) >= 2:
@@ -22,6 +22,7 @@ with open("C:/Users/Vedant/Downloads/postProcessing/postProcessing/forces1/0/for
                 times.append(time)
                 fx_total.append(fx)
                 px_total.append(px)
+                vx_total.append(vx)
                 
             except ValueError:
                 print(f"Error parsing line: {line.strip()}")
@@ -31,22 +32,37 @@ print("Sample times:", times[:5])
 print("Sample total fx values:", fx_total[:5])
 
 
-
-# plt.style.use('_mpl-gallery')
-
 # make data
-x = np.linspace(times[0], times[len(times)-1])
-y = np.linspace(fx_total[0], fx_total[len(fx_total)-1])
-
+x = times
+y = fx_total
+print(fx_total[-1])
 
 # plot
 fig, ax = plt.subplots()
 
-ax.plot(x, y, linewidth=2.0)
+ax.plot(x, y, linewidth=1.3)
 
-ax.set(xlim=(0, 50), xticks=np.arange(1, 8),
-       ylim=(-2000, 2000), yticks=np.arange(1, 8))
+ax.set(xlim=(0, 60), xticks=np.arange(0, 60, 10),
+       ylim=(-1000, 2000), yticks=np.arange(-1000, 2000, 500))
 
-# plt.xlim([0, 50])
-# plt.ylim([-2000, 2000])
 plt.show()
+
+min = []
+min_indice = []
+window = 20
+
+def minIdentify(input_list):
+    input_list = np.array(input_list)
+    print(type(input_list))
+    for i in range(window, len(input_list) - window):
+        # Check if the point is smaller than the 5 points to its left and right
+        left_neighbors = input_list[i - window:i]
+        right_neighbors = input_list[i + 1:i + window + 1]
+        
+        # Check if the current point is smaller than all its neighbors
+        if input_list[i] < np.min(left_neighbors) and input_list[i] < np.min(right_neighbors):
+            min_indice.append(i)
+            min.append(input_list[i])        
+        
+minIdentify(fx_total)
+print(min)
